fa844aebc9cf672f094a5eeea3b091892654cc1d
developerJatin7 <jatinsuryawanshi725@gmail.com>
2026-07-17 19:15:05 +0530

    utils code discussion

--- Backend/gateway-service/src/index.js
+++ Backend/gateway-service/src/index.js
@@ -5,7 +5,16 @@ dotenv.config({
 });
 
 
-connectDatabase();
+connectDatabase()
+.then(()=>{
+	app.listen(process.env.PORT || 8000, () => {
+		console.log(`Server is running on port ${process.env.PORT || 8000}`);
+	})
+})
+.catch((error)=>{
+	console.error("MongoDB connection failed !!!", error);
+	process.exit(1); // Exit the process with an error code
+})
 
 
 
---
+++ Backend/gateway-service/src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch(next)
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+// }
+//     catch (error) {
+//         res.status(error.code || 500)
+//         .json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
