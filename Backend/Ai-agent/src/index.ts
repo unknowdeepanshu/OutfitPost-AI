270c2f9dc9e6dc31a899e9b574b70ceafe1e3ec4
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2026-07-22 15:53:07 +0530

    update the agent serever add routes ,app ,asyncHandler and add genratedimage

--- Backend/Ai-agent/src/index.ts
+++ Backend/Ai-agent/src/index.ts
@@ -1,28 +1,8 @@
-import express from "express";
+import { app } from "./app.ts";
 import dotenv from "dotenv";
-import cors from "cors";
-import type { Request, Response } from "express";
-const app = express();
-dotenv.config();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-// let ThreadIdStore = ["chat-1"];
-
-app.get("/", (_req: Request, res: Response) => {
-  res.json({ ji: "cjec", duck: "doe" });
+dotenv.config({
+  path: "./src/.env",
 });
-app.get("/test", (_req: Request, res: Response) => {
-  res.send("<h1> hi buddy</h1>");
-});
-app.get("/api/enhanceImage", (_req: Request, res: Response) => {
-  res.send("<h1> hi buddy</h1>");
-});
-
-app.get("/api/backgroundReomove", (_req: Request, res: Response) => {
-  res.send("<h1> hi buddy</h1>");
-});
-app.listen(3040, () => {
-  console.log("Server is running on http://localhost:3040");
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 });
