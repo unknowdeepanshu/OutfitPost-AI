ec5d8fe7db43b6d400dd637f736700dd18dc99fe
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2026-07-24 17:30:19 +0530

    add clerk and make protected rourtes

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import { ThemeProvider } from "@/components/Darkmode/theme-provider.tsx";
 import { Provider } from "react-redux";
 import { store } from "./Store/store.ts";
-
+import { ClerkProvider } from "@clerk/react";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Conversion from "@/components/Conversion/conversion.tsx";
 import Dashboard from "@/pages/Dashboard/Dashboard.tsx";
 import App from "@/pages/Landing/App.tsx";
+import ProtectRoutes from "@/components/ProtecteRoutes/ProtecteRoutes.tsx";
+
 const Route = createBrowserRouter([
   {
     path: "/",
@@ -17,17 +19,22 @@ const Route = createBrowserRouter([
   },
   {
     path: "/dashboard",
-    element: <Dashboard />,
+    element: <ProtectRoutes Children={<Dashboard />} />,
     children: [{ path: "project/:threadId", element: <Conversion /> }],
   },
 ]);
-
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
-        <RouterProvider router={Route} />
-      </ThemeProvider>
-    </Provider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Provider store={store}>
+        <ThemeProvider>
+          <RouterProvider router={Route} />
+        </ThemeProvider>
+      </Provider>
+    </ClerkProvider>
   </StrictMode>,
 );
--- Frontend/src/pages/Landing/App.tsx
+++ Frontend/src/pages/Landing/App.tsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router";
-
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 function App() {
   return (
     <>
-      <div>
+      <div className="flex h-screen flex-col items-center justify-center gap-4">
         <Link to={"/dashboard"}>this is landing pages</Link>
+        <header>
+          <Show when="signed-out">
+            <SignInButton />
+            <SignUpButton />
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </header>
       </div>
     </>
   );
