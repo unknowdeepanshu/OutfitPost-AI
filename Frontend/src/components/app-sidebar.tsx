a3b356abafea53c4195c9e41d9a3a0975b01ed01
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2026-07-24 21:12:24 +0530

    delete the icon and add logo outfitPost  Ai

--- Frontend/src/components/app-sidebar.tsx
+++ Frontend/src/components/app-sidebar.tsx
@@ -54,7 +54,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" render={<Link to="/" />}>
               <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                <TerminalIcon className="size-4" />
+                <img src="/OutfitPost_AI.png" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">OutfitPost AI</span>
